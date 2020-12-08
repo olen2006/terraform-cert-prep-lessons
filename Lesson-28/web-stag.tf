5b4609f1b6aefff76b48a8c67a398a2db6006243
Oleg <olen2006@gmail.com>
2020-12-08 06:02:04 +0000

    Refuctoring example, before changes

---
+++ Lesson-28/web-stag.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "web-stag" {
+  ami                    = data.aws_ami.latest_amazon_linux.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.web-prod.id]
+  user_data              = <<EOF
+  #!/bin/bash
+  yum -y update
+  yum -y install httpd
+  myip=`curl http:/169.254.169.254/latest/meta-data/local-ipv4`
+  echo "<h2> STAG WebServer with IP: $myip</h2><br>build by Terraform!" > /var/www/html/index.html
+  sudo service httpd start
+  chkconfig httpd on
+  EOF
+  tags = {
+    Name  = "STAG WebServer"
+    Owner = "Walter Sobchak"
+  }
+}
+resource "aws_security_group" "web-stag" {
+  name        = "WebServer SG Stag"
+  description = "My First SG"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name  = "WebServer SG"
+    Owner = "Walter Sobchak"
+  }
+}

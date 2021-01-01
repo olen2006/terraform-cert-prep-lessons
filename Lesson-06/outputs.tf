04427ee70c3c6b48a461b7bef8e29f7b6522d9a8
Oleg <olen2006@gmail.com>
2020-12-31 20:12:56 -0700

    outputs. Using Attribute Reference from Terraform Documentation

--- Lesson-06/outputs.tf
+++ Lesson-06/outputs.tf
@@ -5,7 +5,7 @@ output "webserver_sg_id" {
   value = aws_security_group.my_webserver.id
 }
 output "webserver_sg_arn" {
-  value = aws_security_group.my_webserver.arn
+  value = aws_security_group.my_webserver.arn #
 }
 output "webserver_public_ip_address" {
   value       = aws_eip.EIP.public_ip
